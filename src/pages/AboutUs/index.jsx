72561fc898d7963c24f4c85e63f31722f56400ef
Abdurahman <abdurahmansamatovic@gmail.com>
2025-02-11 22:50:54 +0600

    fix: map

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -249,8 +249,9 @@ const AboutUs = () => {
       {!md && (
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d17487.545960310345!2d74.6425034748933!3d42.85946636606197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDLCsDUxJzU1LjEiTiA3NMKwMzcnNTUuNyJF!5e0!3m2!1sru!2skg!4v1739282151133!5m2!1sru!2skg"
+          width="100%"
           height={!md ? "389px" : "100%"}
-          style={{ border: "0", borderRadius: "0", marginTop: 32 }}
+          style={{ border: "0", borderRadius: "20px", marginTop: "30px" }}
           allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
